ce0943b070947f79d88bd409b0da5bb0c57e0086
bagasys <bagasys3@gmail.com>
2022-12-17 17:17:36 +0700

    use helmet, cors, xss-clean middlewares

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const helmet = require("helmet");
+const xss = require("xss-clean");
 const routes = require("./routes/v1");
+const cors = require("cors");
+
 const { errorHandler, errorConverter } = require("./middlewares/error");
 
 const app = express();
+
+app.use(helmet());
+
+app.use(xss());
+
+app.use(cors());
+app.options("*", cors());
 
 app.use("/v1", routes);
 
