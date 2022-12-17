c8e5809e24879dbe868a1d95c4d9a57147e0b814
bagasys <bagasys3@gmail.com>
2022-12-17 17:11:35 +0700

    introduce standardize error object and function to convert any errors to it

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const routes = require("./routes/v1");
-const { errorHandler } = require("./middlewares/error");
+const { errorHandler, errorConverter } = require("./middlewares/error");
 
 const app = express();
 
 app.use("/v1", routes);
+
+app.use(errorConverter);
 
 app.use(errorHandler);
 
--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -23,6 +23,20 @@ const errorHandler = (err, req, res, next) => {
   res.status(statusCode).send(response);
 };
 
+const errorConverter = (err, req, res, next) => {
+  let error = err;
+  if (!(error instanceof ApiError)) {
+    const statusCode =
+      error.statusCode || error instanceof mongoose.Error
+        ? httpStatus.BAD_REQUEST
+        : httpStatus.INTERNAL_SERVER_ERROR;
+    const message = error.message || httpStatus[statusCode];
+    error = new ApiError(statusCode, message, false, err.stack);
+  }
+  next(error);
+};
+
 module.exports = {
   errorHandler,
+  errorConverter,
 };
